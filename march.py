432f6f3e1a0c1ea99abe87f3f008990c9d8737ca
Amer Shalan <amer.shalan@gmail.com>
2016-12-12 12:49:02 -0500

    Fix port

--- march.py
+++ march.py
@@ -75,9 +75,7 @@ def send_json(path):
 
 if __name__ == '__main__':
     '''Connects to the server'''
-
-    HOST = '127.0.0.1'
-    PORT = '4000'
-    app.run(HOST, PORT)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 # 74, 49, 0, 23, 7, 25
